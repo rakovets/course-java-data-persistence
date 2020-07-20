75edd57504f9e9a9ee8b876e21c7165d6c01cc67
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-07-20 21:42:01 +0300

    Task 5: create class Dao

--- intro-hibernate/src/main/java/com/rakovets/course/datapersistence/solution/hibernate/dal/dao/Dao.java
+++ intro-hibernate/src/main/java/com/rakovets/course/datapersistence/solution/hibernate/dal/dao/Dao.java
@@ -1,4 +1,13 @@
 package com.rakovets.course.datapersistence.solution.hibernate.dal.dao;
 
+import org.hibernate.Session;
+
 public abstract class Dao<T> {
+    public abstract void createPainting(T object, Session session);
+
+    public abstract T readPainting(T object, Session session);
+
+    public abstract void updatePainting(T object, Session session);
+
+    public abstract void deletePainting(T object, Session session);
 }
