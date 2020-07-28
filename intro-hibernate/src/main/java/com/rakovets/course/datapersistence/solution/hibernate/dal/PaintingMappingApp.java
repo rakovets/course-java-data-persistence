bfef630b45879eb3df3ac66b3e8030340b9a6135
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-07-28 10:55:02 +0300

    Task 8: add logging to PaintingMappingApp, delete unnessesry imports

--- intro-hibernate/src/main/java/com/rakovets/course/datapersistence/solution/hibernate/dal/PaintingMappingApp.java
+++ intro-hibernate/src/main/java/com/rakovets/course/datapersistence/solution/hibernate/dal/PaintingMappingApp.java
@@ -2,12 +2,16 @@ package com.rakovets.course.datapersistence.solution.hibernate.dal;
 
 import com.rakovets.course.datapersistence.solution.hibernate.dal.dao.PaintingDao;
 import com.rakovets.course.datapersistence.solution.hibernate.dal.entity.Painting;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class PaintingMappingApp {
+    static Logger logger = LogManager.getLogger(PaintingMappingApp.class);
+
     public static void main(String[] args) {
         Painting painting = new Painting();
         painting.setName("Wave");
@@ -16,14 +20,23 @@ public class PaintingMappingApp {
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
+        logger.debug("Open session");
+
         Transaction transaction = session.beginTransaction();
+        logger.info("Transaction start");
+
         PaintingDao paintingDao = new PaintingDao();
         paintingDao.createPainting(painting, session);
+        logger.debug("Create painting in DB");
         Painting paintingFromDb = paintingDao.readPainting(1, session);
+        logger.debug("Read painting from DB");
         transaction.commit();
+        logger.info("Transaction commit");
 
         session.close();
+        logger.info("Close session");
         sessionFactory.close();
+        logger.info("Close session factory");
         System.out.println(paintingFromDb);
     }
 }
--- intro-hibernate/src/main/java/com/rakovets/course/datapersistence/solution/hibernate/dal/dao/PaintingDao.java
+++ intro-hibernate/src/main/java/com/rakovets/course/datapersistence/solution/hibernate/dal/dao/PaintingDao.java
@@ -1,13 +1,10 @@
 package com.rakovets.course.datapersistence.solution.hibernate.dal.dao;
 
 import com.rakovets.course.datapersistence.solution.hibernate.dal.entity.Painting;
-import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
-import org.hibernate.annotations.common.util.impl.LoggerFactory;
 
 
 public class PaintingDao extends Dao<Painting> {
-
     @Override
     public void createPainting(Painting painting, Session session) {
         session.save(painting);
