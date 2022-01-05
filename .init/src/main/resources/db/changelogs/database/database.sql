445ffcba2d9623bd87ed7a4f4bf7372e289a8069
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-01-05 18:22:24 +0300

    feat: add DB init for theme 'DAO'

--- .init/src/main/resources/db/changelogs/database/database.sql
+++ .init/src/main/resources/db/changelogs/database/database.sql
@@ -3,13 +3,19 @@
 --changeset rakovets:database-1 dbms:postgresql
 -- precondition
 CREATE SCHEMA IF NOT EXISTS jdbc;
+CREATE SCHEMA IF NOT EXISTS dao;
 --rollback DROP SCHEMA jdbc CASCADE;
+--rollback DROP SCHEMA dao CASCADE;
 
 --changeset rakovets:database-1 dbms:mariadb
 -- precondition
 CREATE SCHEMA IF NOT EXISTS jdbc;
+CREATE SCHEMA IF NOT EXISTS dao;
 --rollback DROP SCHEMA jdbc;
+--rollback DROP SCHEMA dao;
 
 --changeset rakovets:database-2 dbms:mariadb
 GRANT ALL PRIVILEGES ON jdbc.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON dao.* TO 'mariadb'@'%';
 --rollback REVOKE ALL PRIVILEGES ON jdbc.* FROM 'mariadb'@'%';
+--rollback REVOKE ALL PRIVILEGES ON dao.* FROM 'mariadb'@'%';
