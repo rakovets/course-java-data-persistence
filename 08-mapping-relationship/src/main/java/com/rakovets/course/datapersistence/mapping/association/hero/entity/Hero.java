809191abd2f938cbeee627333dd9a4f161868f85
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-01-09 13:49:06 +0300

    refactor: full refactor for project

---
+++ 08-mapping-relationship/src/main/java/com/rakovets/course/datapersistence/mapping/association/hero/entity/Hero.java
@@ -0,0 +1,80 @@
+package com.rakovets.course.datapersistence.mapping.association.hero.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "heroes")
+@NoArgsConstructor
+@ToString
+public class Hero {
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    @Column(name = "name")
+    private String name;
+
+    @Getter
+    @Setter
+    @Column(name = "gender")
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Getter
+    @Setter
+    @Column(name = "creation_time")
+    private LocalDateTime creationTime;
+
+    @Getter
+    @Setter
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "meleeWeapon", column = @Column(name = "first_melee_weapon")),
+            @AttributeOverride(name = "rangedWeapon", column = @Column(name = "first_ranged_weapon"))
+    })
+    private EquipmentSet firstEquipmentSet;
+
+    @Getter
+    @Setter
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "meleeWeapon", column = @Column(name = "second_melee_weapon")),
+            @AttributeOverride(name = "rangedWeapon", column = @Column(name = "second_ranged_weapon"))
+    })
+    private EquipmentSet secondEquipmentSet;
+
+    @OneToOne(mappedBy = "hero")
+    @Getter
+    @Setter
+    private Inventory inventory;
+
+    @ManyToMany(mappedBy = "heroes")
+    @Getter
+    @Setter
+    private Set<Quest> quests = new HashSet<>();
+}
