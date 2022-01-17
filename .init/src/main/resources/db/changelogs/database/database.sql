b2887337f9aedd89b9653885359bcaf580a4a586
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-01-17 06:57:25 +0300

    feat: update init DB

--- .init/src/main/resources/db/changelogs/database/database.sql
+++ .init/src/main/resources/db/changelogs/database/database.sql
@@ -6,10 +6,14 @@ CREATE SCHEMA IF NOT EXISTS jdbc;
 CREATE SCHEMA IF NOT EXISTS dao;
 CREATE SCHEMA IF NOT EXISTS intro_jpa;
 CREATE SCHEMA IF NOT EXISTS intro_hibernate;
+CREATE SCHEMA IF NOT EXISTS mapping_basic;
+CREATE SCHEMA IF NOT EXISTS mapping_inheritance;
 --rollback DROP SCHEMA jdbc CASCADE;
 --rollback DROP SCHEMA dao CASCADE;
 --rollback DROP SCHEMA intro_jpa CASCADE;
 --rollback DROP SCHEMA intro_hibernate CASCADE;
+--rollback DROP SCHEMA mapping_basic CASCADE;
+--rollback DROP SCHEMA mapping_inheritance CASCADE;
 
 --changeset rakovets:database-1 dbms:mariadb
 -- precondition
@@ -17,17 +21,25 @@ CREATE SCHEMA IF NOT EXISTS jdbc;
 CREATE SCHEMA IF NOT EXISTS dao;
 CREATE SCHEMA IF NOT EXISTS intro_jpa;
 CREATE SCHEMA IF NOT EXISTS intro_hibernate;
+CREATE SCHEMA IF NOT EXISTS mapping_basic;
+CREATE SCHEMA IF NOT EXISTS mapping_inheritance;
 --rollback DROP SCHEMA jdbc;
 --rollback DROP SCHEMA dao;
 --rollback DROP SCHEMA intro_jpa;
 --rollback DROP SCHEMA intro_hibernate;
+--rollback DROP SCHEMA mapping_basic;
+--rollback DROP SCHEMA mapping_inheritance;
 
 --changeset rakovets:database-2 dbms:mariadb
 GRANT ALL PRIVILEGES ON jdbc.* TO 'mariadb'@'%';
 GRANT ALL PRIVILEGES ON dao.* TO 'mariadb'@'%';
 GRANT ALL PRIVILEGES ON intro_jpa.* TO 'mariadb'@'%';
 GRANT ALL PRIVILEGES ON intro_hibernate.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON mapping_basic.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON mapping_inheritance.* TO 'mariadb'@'%';
 --rollback REVOKE ALL PRIVILEGES ON jdbc.* FROM 'mariadb'@'%';
 --rollback REVOKE ALL PRIVILEGES ON dao.* FROM 'mariadb'@'%';
 --rollback REVOKE ALL PRIVILEGES ON intro_jpa.* FROM 'mariadb'@'%';
 --rollback REVOKE ALL PRIVILEGES ON intro_hibernate.* FROM 'mariadb'@'%';
+--rollback REVOKE ALL PRIVILEGES ON mapping_basic.* FROM 'mariadb'@'%';
+--rollback REVOKE ALL PRIVILEGES ON mapping_inheritance.* FROM 'mariadb'@'%';
